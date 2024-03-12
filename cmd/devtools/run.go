646887821254b8805729387cf7d5b2f3ed515d59
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-03-12 14:12:40 -0600

    added running cometbft to tview ui

--- cmd/devtools/run.go
+++ cmd/devtools/run.go
@@ -100,73 +100,6 @@ func checkIfInitialized(path string) bool {
 	return status
 }
 
-// TODO: are there any other terminals that should be supported?
-// var terminalEmulators = []struct {
-// 	command string
-// 	args    []string
-// }{
-// 	{"x-terminal-emulator", []string{"-e"}}, // Debian alternatives system
-// 	{"gnome-terminal", []string{"--"}},      // GNOME
-// 	{"konsole", []string{"-e"}},             // KDE
-// 	{"xfce4-terminal", []string{"-e"}},      // XFCE
-// 	{"lxterminal", []string{"-e"}},          // LXDE
-// 	{"mate-terminal", []string{"-e"}},       // MATE
-// 	{"terminator", []string{"-e"}},          // Terminator
-// 	{"tilix", []string{"-e"}},               // Tilix
-// 	{"xterm", []string{"-e"}},               // XTerm
-// }
-
-// openTerminal attempts to open a new terminal window running the specified command.
-// func runLinuxCommand(command string) bool {
-// 	for _, emulator := range terminalEmulators {
-// 		if path, err := exec.LookPath(emulator.command); err == nil {
-// 			// Command found, attempt to execute it
-// 			args := append(emulator.args, command)
-// 			cmd := exec.Command(path, args...)
-// 			if err := cmd.Start(); err == nil {
-// 				// Successfully started the terminal emulator
-// 				return true
-// 			}
-// 		}
-// 	}
-// 	return false // No known terminal emulator found or succeeded in opening
-// }
-
-// func executeCommand(cmdIn string, env []string) {
-// 	var cmd *exec.Cmd
-
-// 	switch runtime.GOOS {
-// 	case "darwin":
-// 		// TODO: finish fixing the extra terminal window issue OR just move on
-// 		// to the TUI
-// 		// fullCmd := `tell application "Terminal"
-// 		// 	if (count of windows) = 1 then
-// 		// 		tell application "Terminal" to do script "` + cmdIn + `" in window 1
-// 		// 	else
-// 		// 		tell application "Terminal" to do script "` + cmdIn + `"
-// 		// 	end if
-// 		// end tell
-// 		// `
-// 		// cmd = exec.Command("osascript", "-e", fullCmd)
-// 		cmd = exec.Command("osascript", "-e", `tell application "Terminal" to do script "`+cmdIn+`"`)
-
-// 	case "linux":
-// 		didRun := runLinuxCommand(cmdIn)
-// 		if !didRun {
-// 			panic("No terminal emulator found")
-// 		}
-
-// 	default:
-// 		panic("Unsupported OS")
-// 	}
-// 	cmd.Env = env
-
-// 	err := cmd.Start()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
 func run() {
 	// TODO: make the home dir name configuratble
 	homePath, err := os.UserHomeDir()
@@ -226,6 +159,7 @@ func run() {
 			app.Draw()
 		})
 	sequencerTextView.SetTitle(" Sequencer ").SetBorder(true)
+	// sequencerTextView.SetScrollToEnd(true)
 
 	// create text view object for the cometbft
 	cometbftTextView := tview.NewTextView().
@@ -252,25 +186,11 @@ func run() {
 		})
 	conductorTextView.SetTitle(" Conductor ").SetBorder(true)
 
-	// get the HOME path
-	// TODO: make this configureable
-	// homePath, err := os.UserHomeDir()
-	// if err != nil {
-	// 	fmt.Println("could not get home dir", err)
-	// 	return
-	// }
-
-	// envPath := filepath.Join(homePath, ".astria/local-dev-astria/.env")
-	// environment := loadAndGetEnvVariables(envPath)
-
 	// Run a command and stream its output to the TextView.
 	// go func() for running the sequencer
 	go func() {
 		binPath := filepath.Join(homePath, ".astria/local-dev-astria/astria-sequencer")
 
-		// for _, env := range environment {
-		// 	fmt.Println(env)
-		// }
 		seqCmd := exec.Command(binPath)
 		seqCmd.Env = environment
 
@@ -302,16 +222,15 @@ func run() {
 
 		// Create a scanner to read the output line by line.
 		// TODO: read both stdout and stderr
-		// output := io.MultiReader(stdout, stderr)
-		// scanner := bufio.NewScanner(output)
-		// scanner := bufio.NewScanner(stderr)
 		// stderrScanner := bufio.NewScanner(stderr)
 		stdoutScanner := bufio.NewScanner(stdout)
+		// output := io.MultiReader(stdout, stderr)
 
 		for stdoutScanner.Scan() {
 			line := stdoutScanner.Text()
 			app.QueueUpdateDraw(func() {
 				sequencerTextView.Write([]byte(line + "\n"))
+				sequencerTextView.ScrollToEnd()
 			})
 		}
 		if err := stdoutScanner.Err(); err != nil {
@@ -325,208 +244,147 @@ func run() {
 
 	// go func() for running the cometbft
 	go func() {
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.Write([]byte("entered\n"))
-		// })
 		cometbftDataPath := filepath.Join(homePath, ".astria/data/.cometbft")
 		cometbftCmdPath := filepath.Join(homePath, ".astria/local-dev-astria/cometbft")
 		initCmdArgs := []string{"init", "--home", cometbftDataPath}
-		// p := fmt.Sprintf("Running: `%v`\n", initCmdPath)
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.Write([]byte(p))
-		// })
 		initCmd := exec.Command(cometbftCmdPath, initCmdArgs...)
-		// initCmd := exec.Command(initCmdPath)
 		initCmd.Env = environment
+
+		p := fmt.Sprintf("Running command `%v %v %v %v`\n", initCmd, initCmdArgs[0], initCmdArgs[1], initCmdArgs[2])
+		app.QueueUpdateDraw(func() {
+			cometbftTextView.Write([]byte(p))
+			cometbftTextView.ScrollToEnd()
+		})
 
 		out, err := initCmd.CombinedOutput()
 		if err != nil {
 			p := fmt.Sprintf("Error executing command `%v`: %v\n", initCmd, err)
 			app.QueueUpdateDraw(func() {
 				cometbftTextView.Write([]byte(p))
+				cometbftTextView.ScrollToEnd()
+
 			})
 			return
 		}
-		// p := fmt.Sprintf("Output of command `%v`: %s\n", initCmd, out)
 		app.QueueUpdateDraw(func() {
 			cometbftTextView.Write([]byte(out))
+			cometbftTextView.ScrollToEnd()
+
 		})
 
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.Write([]byte("command created\n"))
-		// })
+		// $ cp genesis.json ../data/.cometbft/config/genesis.json
+		initGenesisJsonPath := filepath.Join(homePath, ".astria/local-dev-astria/genesis.json")
+		endGenesisJsonPath := filepath.Join(homePath, ".astria/data/.cometbft/config/genesis.json")
+		copyArgs := []string{initGenesisJsonPath, endGenesisJsonPath}
+		copyCmd := exec.Command("cp", copyArgs...)
+		copyCmd.Env = environment
 
-		// stdout, err := initCmd.StdoutPipe()
-		// if err != nil {
-		// 	panic(err)
-		// }
+		_, err = copyCmd.CombinedOutput()
+		if err != nil {
+			p := fmt.Sprintf("Error executing command `%v`: %v\n", copyCmd, err)
+			app.QueueUpdateDraw(func() {
+				cometbftTextView.Write([]byte(p))
+				cometbftTextView.ScrollToEnd()
 
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.Write([]byte("scanner created\n"))
-		// })
-		// stdoutScanner := bufio.NewScanner(stdout)
+			})
+			return
+		}
+		p = fmt.Sprintf("Copied genesis.json to %s\n", endGenesisJsonPath)
+		app.QueueUpdateDraw(func() {
+			cometbftTextView.Write([]byte(p))
+			cometbftTextView.ScrollToEnd()
 
-		// if err := initCmd.Run(); err != nil {
-		// 	panic(err)
-		// }
-		// if err := initCmd.Start(); err != nil {
-		// 	panic(err)
-		// }
-
-		// for stdoutScanner.Scan() {
-		// 	line := stdoutScanner.Text()
-		// 	app.QueueUpdateDraw(func() {
-		// 		cometbftTextView.Write([]byte(line + "\n"))
-		// 	})
-		// }
-		// if err := stdoutScanner.Err(); err != nil {
-		// 	panic(err)
-		// }
-		// if err := initCmd.Wait(); err != nil {
-		// 	panic(err)
-		// }
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.Write([]byte("done\n"))
-		// })
-
-		// app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		// 	if event.Key() == tcell.KeyCtrlC {
-		// 		if err := initCmd.Process.Signal(syscall.SIGINT); err != nil {
-		// 			fmt.Println("Failed to send SIGINT to the process:", err)
-		// 		}
-		// 		// app.Stop()
-		// 		return nil
-		// 	}
-		// 	return event
-		// })
-
-		// stderr, err := initCmd.StderrPipe()
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// stdout, err := initCmd.StdoutPipe()
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		// if err := initCmd.Start(); err != nil {
-		// 	panic(err)
-		// }
-		// stdoutScanner := bufio.NewScanner(stdout)
-		// stdoutScanner := bufio.NewScanner(stderr)
-
-		// for stdoutScanner.Scan() {
-		// for stdoutScanner.Scan() {
-		// line := stdoutScanner.Text()
-		// app.QueueUpdateDraw(func() {
-		// 	cometbftTextView.SetText("testing\n")
-		// 	// cometbftTextView.Write([]byte(line + "\n"))
-		// })
-		// }
-		// if err := stdoutScanner.Err(); err != nil {
-		// 	panic(err)
-		// }
-
-		// if err := initCmd.Run(); err != nil {
-		// 	panic(err)
-		// }
-		// 	executeFiniteCommand(app, initCmd, cometbftTextView)
-
-		// // $ cp genesis.json ../data/.cometbft/config/genesis.json
-		// initGenesisJsonPath := filepath.Join(homePath, ".astria/local-dev-astria/genesis.json")
-		// endGenesisJsonPath := filepath.Join(homePath, ".astria/data/.cometbft/config/genesis.json")
-		// copyGenesisJsonPath := "cp " + initGenesisJsonPath + " " + endGenesisJsonPath
-		// // run cp command here
-
-		// executeFiniteCommand(app, cmd*exec.Cmd, textView*tview.TextView)
+		})
 
 		// // $ cp priv_validator_key.json
-		// initPrivValidatorJsonPath := filepath.Join(homePath, ".astria/local-dev-astria/priv_validator_key.json")
-		// endPrivValidatorJsonPath := filepath.Join(homePath, ".astria/data/.cometbft/config/priv_validator_key.json")
-		// copyPrivValidatorJsonPath := "cp " + initPrivValidatorJsonPath + " " + endPrivValidatorJsonPath
-		// // run cp command here
+		initPrivValidatorJsonPath := filepath.Join(homePath, ".astria/local-dev-astria/priv_validator_key.json")
+		endPrivValidatorJsonPath := filepath.Join(homePath, ".astria/data/.cometbft/config/priv_validator_key.json")
+		copyArgs = []string{initPrivValidatorJsonPath, endPrivValidatorJsonPath}
+		copyCmd = exec.Command("cp", copyArgs...)
+		copyCmd.Env = environment
 
-		// // $ sed -i '.bak' 's/timeout_commit = \\\"1s\\\"/timeout_commit = \\\"2s\\\"/g' ../data/.cometbft/config/config.toml
-		// cometbftConfigPath := filepath.Join(homePath, ".astria/data/.cometbft/config/config.toml")
-		// cometbftSedCommand := "sed -i '.bak' 's/timeout_commit = \\\"1s\\\"/timeout_commit = \\\"2s\\\"/g' " + cometbftConfigPath
-		// // run sed command here
+		_, err = copyCmd.CombinedOutput()
+		if err != nil {
+			p := fmt.Sprintf("Error executing command `%v`: %v\n", copyCmd, err)
+			app.QueueUpdateDraw(func() {
+				cometbftTextView.Write([]byte(p))
+				cometbftTextView.ScrollToEnd()
 
+			})
+			return
+		}
+		p = fmt.Sprintf("Copied priv_validator_key.json to %s\n", endPrivValidatorJsonPath)
+		app.QueueUpdateDraw(func() {
+			cometbftTextView.Write([]byte(p))
+			cometbftTextView.ScrollToEnd()
+
+		})
+
+		// go code for the following sed command
+		// $ sed -i '.bak' 's/timeout_commit = \\\"1s\\\"/timeout_commit = \\\"2s\\\"/g' ../data/.cometbft/config/config.toml
+		cometbftConfigPath := filepath.Join(homePath, ".astria/data/.cometbft/config/config.toml")
+		oldValue := `timeout_commit = "1s"`
+		newValue := `timeout_commit = "2s"`
+
+		if err := replaceInFile(cometbftConfigPath, oldValue, newValue); err != nil {
+			p := fmt.Sprintf("Error updating the file: %v : %v", cometbftConfigPath, err)
+			app.QueueUpdateDraw(func() {
+				cometbftTextView.Write([]byte(p))
+				cometbftTextView.ScrollToEnd()
+
+			})
+			return
+		} else {
+			p := fmt.Sprintf("Updated %v successfully", cometbftConfigPath)
+			app.QueueUpdateDraw(func() {
+				cometbftTextView.Write([]byte(p))
+				cometbftTextView.ScrollToEnd()
+
+			})
+		}
+
+		// binPath := filepath.Join(homePath, ".astria/local-dev-astria/astria-sequencer")
 		// // actual run command - ./cometbft node --home ../data/.cometbft
-		// binPath := filepath.Join(homePath, ".astria/local-dev-astria/cometbft")
-		// // run the actual command
+		nodeCmdArgs := []string{"node", "--home", cometbftDataPath}
+		cometbftCmd := exec.Command(cometbftCmdPath, nodeCmdArgs...)
+		cometbftCmd.Env = environment
 
-		// initCmd := exec.Command(initCmdPath)
+		stdout, err := cometbftCmd.StdoutPipe()
+		// stdout, err := cometbftCmd.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
 
-		// envPath := filepath.Join(homePath, ".astria/local-dev-astria/.env")
-		// environment := loadAndGetEnvVariables(envPath)
+		if err := cometbftCmd.Start(); err != nil {
+			panic(err)
+		}
 
-		// for _, env := range environment {
-		// 	fmt.Println(env)
-		// }
-		// cmd = exec.Command(binPath)
-		// cmd.Env = environment
+		stdoutScanner := bufio.NewScanner(stdout)
 
-		// app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		// 	if event.Key() == tcell.KeyCtrlC {
-		// 		if err := cmd.Process.Signal(syscall.SIGINT); err != nil {
-		// 			fmt.Println("Failed to send SIGINT to the process:", err)
-		// 		}
-		// 		app.Stop()
-		// 		return nil
-		// 	}
-		// 	return event
-		// })
+		for stdoutScanner.Scan() {
+			line := stdoutScanner.Text()
+			app.QueueUpdateDraw(func() {
+				cometbftTextView.Write([]byte(line + "\n"))
+				cometbftTextView.ScrollToEnd()
 
-		// stdout, err := cmd.StdoutPipe()
-		// if err != nil {
-		// 	panic(err)
-		// }
+			})
+		}
+		if err := stdoutScanner.Err(); err != nil {
+			panic(err)
+		}
 
-		// if err := cmd.Start(); err != nil {
-		// 	panic(err)
-		// }
-
-		// stdoutScanner := bufio.NewScanner(stdout)
-
-		// for stdoutScanner.Scan() {
-		// 	line := stdoutScanner.Text()
-		// 	app.QueueUpdateDraw(func() {
-		// 		sequencerTextView.Write([]byte(line + "\n"))
-		// 	})
-		// }
-
-		// app.QueueUpdateDraw(func() {
-		// 	sequencerTextView.Write([]byte("we finished" + "\n"))
-		// })
-		// if err := stdoutScanner.Err(); err != nil {
-		// 	panic(err)
-		// }
-
-		// if err := cmd.Wait(); err != nil {
-		// 	panic(err)
-		// }
+		if err := cometbftCmd.Wait(); err != nil {
+			panic(err)
+		}
 	}()
 
 	// Create a new Flex layout.
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(sequencerTextView, 0, 1, false).
 		AddItem(cometbftTextView, 0, 1, false).
-		// AddItem(tview.NewBox().SetBorder(true).SetTitle(" 2 "), 0, 1, false).
-		// AddItem(tview.NewBox().SetBorder(true).SetTitle(" 3 "), 0, 1, false).
 		AddItem(composerTextView, 0, 1, false).
-		// AddItem(tview.NewBox().SetBorder(true).SetTitle(" 4 "), 0, 1, false)
 		AddItem(conductorTextView, 0, 1, false)
-		// textview.SetBorder(true).SetTitle("Astria")
-		// if err := box.SetRoot(textView, true).Run(); err != nil {
-		// 	panic(err)
-		// }
 
-		// frame := tview.NewFrame(flex).
-		// 	SetBorders(0, 0, 0, 0, 0, 0).
-		// 	SetBorder(true).
-		// 	SetTitle(" Astria Dev ")
-
-		// if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
@@ -545,4 +403,36 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func replaceInFile(filename, oldValue, newValue string) error {
+	// Read the original file.
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("failed to read the file: %w", err)
+	}
+
+	// Perform the replacement.
+	modifiedContent := strings.ReplaceAll(string(content), oldValue, newValue)
+
+	// Write the modified content to a new temporary file.
+	tmpFilename := filename + ".tmp"
+	if err := os.WriteFile(tmpFilename, []byte(modifiedContent), 0666); err != nil {
+		return fmt.Errorf("failed to write to temporary file: %w", err)
+	}
+
+	// Rename the original file to filename.bak.
+	backupFilename := filename + ".bak"
+	if err := os.Rename(filename, backupFilename); err != nil {
+		return fmt.Errorf("failed to rename original file to backup: %w", err)
+	}
+
+	// Rename the temporary file to the original file name.
+	if err := os.Rename(tmpFilename, filename); err != nil {
+		// Attempt to restore the original file if renaming fails.
+		os.Rename(backupFilename, filename)
+		return fmt.Errorf("failed to rename temporary file to original: %w", err)
+	}
+
+	return nil
 }
