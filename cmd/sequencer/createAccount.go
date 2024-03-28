ca51b6128e9e9d4a39190b1afbe0ccdd9598cbcf
Jesse Snyder <jessetsnyder@gmail.com>
2024-03-28 17:51:26 -0600

    added tabular and json outputs

--- cmd/sequencer/createAccount.go
+++ cmd/sequencer/createAccount.go
@@ -1,10 +1,13 @@
 package sequencer
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/astria/astria-cli-go/internal/sequencer"
+	"github.com/pterm/pterm"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -19,18 +22,41 @@ transactions and blocks. The account will be created with a private key, public 
 
 func init() {
 	sequencerCmd.AddCommand(createAccountCmd)
+
+	createAccountCmd.Flags().Bool("json", false, "Output the account information in JSON format.")
 }
 
 func runCreateAccountCmd(cmd *cobra.Command, args []string) {
+	printJSON := cmd.Flag("json").Value.String() == "true"
+
 	account, err := sequencer.CreateAccount()
 	if err != nil {
 		fmt.Println("Error creating account:", err)
 		os.Exit(1)
 	}
-	// FIXME - improve output. this is just a placeholder for now.
-	fmt.Println("Created account:")
-	fmt.Println("  Private Key:", account.PrivateKey)
-	fmt.Println("  Public Key: ", account.PublicKey)
-	fmt.Println("  Address:    ", account.Address)
-	os.Exit(0)
+
+	// TODO - abstract table and json printing logic to helper functions
+	if printJSON {
+		obj := map[string]string{
+			"address":     account.Address,
+			"public_key":  account.PublicKey,
+			"private_key": account.PrivateKey,
+		}
+		j, err := json.MarshalIndent(obj, "", "  ")
+		if err != nil {
+			fmt.Println("Error marshalling account to JSON:", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(j))
+	} else {
+		header := []string{"Address", "Public Key", "Private Key"}
+		row := []string{account.Address, account.PublicKey, account.PrivateKey}
+		data := pterm.TableData{header, row}
+		output, err := pterm.DefaultTable.WithHasHeader().WithData(data).Srender()
+		if err != nil {
+			log.WithError(err).Error("Error rendering table")
+			os.Exit(1)
+		}
+		pterm.Println(output)
+	}
 }
