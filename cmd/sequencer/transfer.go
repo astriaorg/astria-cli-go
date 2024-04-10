c652f739972bad5c2d02ebf5c3bb70a8614053fd
Jesse Snyder <jessetsnyder@gmail.com>
2024-04-10 13:43:19 -0600

    wip transfer command

---
+++ cmd/sequencer/transfer.go
@@ -0,0 +1,47 @@
+package sequencer
+
+import (
+	"strconv"
+
+	"github.com/astria/astria-cli-go/cmd"
+	"github.com/astria/astria-cli-go/internal/sequencer"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var transferCmd = &cobra.Command{
+	Use:    "transfer [from] [to] [amount]",
+	Short:  "Transfer tokens from one account to another.",
+	Args:   cobra.ExactArgs(3),
+	PreRun: cmd.SetLogLevel,
+	Run:    runTransfer,
+}
+
+func init() {
+	sequencerCmd.AddCommand(transferCmd)
+	transferCmd.Flags().String("url", DefaultSequencerURL, "The URL of the sequencer.")
+}
+
+func runTransfer(cmd *cobra.Command, args []string) {
+	from := args[0]
+	to := args[1]
+	amount, err := strconv.Atoi(args[2])
+	if err != nil {
+		log.WithError(err).Error("Error converting amount to integer")
+		panic(err)
+	}
+
+	url := cmd.Flag("url").Value.String()
+
+	opts := sequencer.TransferOpts{
+		SequencerURL: url,
+		FromKey:      []byte(from),
+		ToAddress:    []byte(to),
+		Amount:       int(amount),
+	}
+	err = sequencer.Transfer(opts)
+	if err != nil {
+		log.WithError(err).Error("Error transferring tokens")
+		panic(err)
+	}
+}
--- internal/sequencer/sequencer.go
+++ internal/sequencer/sequencer.go
@@ -3,10 +3,13 @@ package sequencer
 import (
 	"context"
 	"encoding/hex"
+	"fmt"
 	"time"
 
+	primitivev1 "buf.build/gen/go/astria/astria/protocolbuffers/go/astria/primitive/v1"
 	log "github.com/sirupsen/logrus"
 
+	sqproto "buf.build/gen/go/astria/astria/protocolbuffers/go/astria/sequencer/v1"
 	"github.com/astriaorg/go-sequencer-client/client"
 )
 
@@ -93,4 +96,73 @@ func GetBlockheight(sequencerURL string) (int64, error) {
 
 	log.Debug("Blockheight: ", blockheight)
 	return blockheight, nil
+}
+
+// TransferOpts are the options for the Transfer function.
+type TransferOpts struct {
+	// SequencerURL is the URL of the sequencer
+	SequencerURL string
+	// FromKey is the private key of the sender
+	FromKey []byte
+	// ToAddress is the address of the receiver
+	ToAddress []byte
+	// Amount is the amount to be transferred
+	Amount int
+}
+
+// Transfer transfers an amount from one address to another.
+func Transfer(opts TransferOpts) error {
+	opts.SequencerURL = addPortToURL(opts.SequencerURL)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// transaction
+	// FIXME - support bigger numbers
+	amount := &primitivev1.Uint128{
+		Lo: uint64(opts.Amount),
+		Hi: 0,
+	}
+	tx := &sqproto.UnsignedTransaction{
+		Nonce: 1, // FIXME - do i need to get the nonce first? where from?
+		Actions: []*sqproto.Action{
+			{
+				Value: &sqproto.Action_TransferAction{
+					TransferAction: &sqproto.TransferAction{
+						To:         opts.ToAddress,
+						Amount:     amount,
+						AssetId:    []byte("nria"), // FIXME - is this right? put in constant
+						FeeAssetId: []byte("nria"), // FIXME - is this right? put in constant
+					},
+				},
+			},
+		},
+	}
+
+	// client
+	log.Debug("Creating CometBFT client with url: ", opts.SequencerURL)
+	c, err := client.NewClient(opts.SequencerURL)
+	if err != nil {
+		log.WithError(err).Error("Error creating sequencer client")
+		return err
+	}
+
+	// signer
+	signer := client.NewSigner(opts.FromKey)
+	signed, err := signer.SignTransaction(tx)
+	if err != nil {
+		log.WithError(err).Error("Error signing transaction")
+		return err
+	}
+
+	// broadcast tx
+	resp, err := c.BroadcastTxSync(ctx, signed)
+	if err != nil {
+		log.WithError(err).Error("Error broadcasting transaction")
+		return err
+	}
+
+	// FIXME - return something else here and handle output in the cmd's code
+	fmt.Println(resp)
+	return nil
 }
