4843af8cf06802370279c25c9e1fda211e749cbb
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-04-10 13:09:19 -0600

    update clean command

--- cmd/devtools/clean.go
+++ cmd/devtools/clean.go
@@ -10,13 +10,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var DeleteLogs bool
+var DeleteData bool
+
 // cleanCmd represents the clean command
 var cleanCmd = &cobra.Command{
 	Use:    "clean",
-	Short:  "Delete the local development instance data, excluding binaries.",
-	Long:   `Delete the local development instance data. Does not delete the binaries or the config files.`,
+	Short:  "Delete the local development instance data and logs, excluding binaries and config.",
+	Long:   `Delete the local development instance data and logs. Does not delete the binaries or the config files. If no flags are provided, everything will be deleted. If flags are provided, only the selected items will be deleted.`,
 	PreRun: cmd.SetLogLevel,
 	Run:    runClean,
+}
+
+func init() {
+	// top level command
+	devCmd.AddCommand(cleanCmd)
+	cleanCmd.Flags().StringP("instance", "i", DefaultInstanceName, "Choose the instance that will be cleaned.")
+	cleanCmd.Flags().BoolVar(&DeleteLogs, "logs", false, "Delete log files.")
+	cleanCmd.Flags().BoolVar(&DeleteData, "data", false, "Delete local data.")
+
+	// subcommands
+	cleanCmd.AddCommand(allCmd)
 }
 
 func runClean(cmd *cobra.Command, args []string) {
@@ -32,16 +46,38 @@ func runClean(cmd *cobra.Command, args []string) {
 	defaultDir := filepath.Join(homePath, ".astria")
 	instanceDir := filepath.Join(defaultDir, instance)
 	dataDir := filepath.Join(instanceDir, DataDirName)
+	logsDir := filepath.Join(instanceDir, LogsDirName)
 
-	log.Infof("Deleting data for instance '%s'", instance)
-	rmCmd := exec.Command("rm", "-rf", dataDir)
-	if err := rmCmd.Run(); err != nil {
-		log.WithError(err).Error("Error running rm")
-		panic(err)
+	// If no flags are set, delete everything
+	if !DeleteLogs && !DeleteData {
+		DeleteLogs = true
+		DeleteData = true
 	}
 
-	log.Infof("Recreating data dir for instance '%s'", instance)
-	CreateDirOrPanic(dataDir)
+	// If selected, delete logs
+	if DeleteLogs {
+		log.Infof("Deleting logs for instance '%s'", instance)
+		rmCmd := exec.Command("rm", "-rf", logsDir)
+		if err := rmCmd.Run(); err != nil {
+			log.WithError(err).Error("Error running rm")
+			panic(err)
+		}
+		log.Infof("Recreating data dir for instance '%s'", instance)
+		CreateDirOrPanic(logsDir)
+	}
+
+	// If selected, delete data
+	if DeleteData {
+		log.Infof("Deleting data for instance '%s'", instance)
+		rmCmd := exec.Command("rm", "-rf", dataDir)
+		if err := rmCmd.Run(); err != nil {
+			log.WithError(err).Error("Error running rm")
+			panic(err)
+		}
+		log.Infof("Recreating data dir for instance '%s'", instance)
+		CreateDirOrPanic(dataDir)
+		initCometbft(instanceDir, DataDirName, BinariesDirName, LocalConfigDirName)
+	}
 }
 
 var allCmd = &cobra.Command{
@@ -68,13 +104,4 @@ func runCleanAll(cmd *cobra.Command, args []string) {
 		log.WithError(err).Error("Error running rm")
 		panic(err)
 	}
-}
-
-func init() {
-	// top level command
-	devCmd.AddCommand(cleanCmd)
-	cleanCmd.Flags().StringP("instance", "i", DefaultInstanceName, "Choose the instance that will be cleaned.")
-
-	// subcommands
-	cleanCmd.AddCommand(allCmd)
 }
