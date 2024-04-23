38ed2485e1c6adf14656ba82dbde5e98b6b8f0d0
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-04-23 15:34:45 -0600

    add test for processrunner GetInfo()

--- internal/processrunner/processrunner_test.go
+++ internal/processrunner/processrunner_test.go
@@ -150,3 +150,21 @@ func TestProcessRunnerRestart(t *testing.T) {
 	// check that the output contains the expected restart message
 	assert.Contains(t, o2, "[black:white][astria-go] Test process restarted[-:-]\n")
 }
+
+func TestProcessRunnerInfo(t *testing.T) {
+	ctx := context.Background()
+	opts := NewProcessRunnerOpts{
+		Title:   "TestPR",
+		BinPath: "/bin/sleep",
+		Args:    []string{"1"},
+		EnvPath: "",
+	}
+	pr := NewProcessRunner(ctx, opts)
+
+	// get the info text
+	infoText := pr.GetInfo()
+	// set the exact expected output (including spaces for alignment)
+	expectedOutput := " TestPR binary path: /bin/sleep\n Environment path:   \n"
+
+	assert.Equal(t, expectedOutput, infoText, "Info text should match expected output")
+}
