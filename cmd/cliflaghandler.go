9bcb9bef55dd35d33d4cd842ef9e4b40db4ed562
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-05-17 12:20:59 -0600

    update flagHandler GetValue function to properly return differently typed flag values

--- cmd/cliflaghandler.go
+++ cmd/cliflaghandler.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"reflect"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -70,14 +71,37 @@ func (f *CliFlagHandler) getEnvVar(flagName string) string {
 }
 
 // GetValue returns the value of a flag and logs the source of the value. It
-// will panic if the flag does not exist.
+// will panic if the flag does not exist or if the flag cannot be read.
 func (f *CliFlagHandler) GetValue(flagName string) string {
 	envSuffix := strings.ToUpper(strings.ReplaceAll(flagName, "-", "_"))
-	value := viper.GetString(envSuffix)
-	exists := f.Cmd.Flag(flagName)
-	if exists == nil {
+	// confirm the flag exists
+	flag := f.Cmd.Flags().Lookup(flagName)
+	if flag == nil {
 		log.Errorf("Flag '%s' doesn't exist. Has it been bound?", flagName)
 		panic(fmt.Sprintf("getValue: flag doesn't exist: %s", flagName))
+	}
+
+	// get the value from viper based on type
+	var value string
+	valueKind := reflect.TypeOf(flag.Value).Elem().Kind()
+	switch valueKind {
+	case reflect.Bool:
+		// we need to rebind the bool flag to viper when reading the value to
+		// ensure that it is read correctly. Otherwise, viper will always
+		// return the default value.
+		err := viper.BindPFlag(flagName, flag)
+		if err != nil {
+			log.Fatalf("getValu: Error rebinding bool flag for reading: %s", flagName)
+			panic(err)
+		}
+		value = fmt.Sprintf("%t", viper.GetBool(envSuffix))
+
+	case reflect.String:
+		value = viper.GetString(envSuffix)
+
+	default:
+		log.Errorf("Flag '%s' has an unsupported type: %s", flagName, valueKind)
+		panic(fmt.Sprintf("getValue: unsupported flag type: %s", valueKind))
 	}
 
 	if f.Cmd.Flag(flagName).Changed {
