f65404fc0a52244f867aff0542b4c7e2db076cf7
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-05-24 10:37:08 -0600

    bump sequencer version

--- cmd/devtools/config/binaries_config_darwin_amd64.go
+++ cmd/devtools/config/binaries_config_darwin_amd64.go
@@ -9,7 +9,7 @@ type Binary struct {
 
 var Binaries = []Binary{
 	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v0.38.6/cometbft_0.38.6_darwin_amd64.tar.gz"},
-	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v0.12.0/astria-sequencer-x86_64-apple-darwin.tar.gz"},
+	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v0.13.0/astria-sequencer-x86_64-apple-darwin.tar.gz"},
 	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v0.7.0/astria-composer-x86_64-apple-darwin.tar.gz"},
 	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v0.16.0/astria-conductor-x86_64-apple-darwin.tar.gz"},
 }
