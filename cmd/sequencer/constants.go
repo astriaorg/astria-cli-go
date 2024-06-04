2762aaef0fd112a52b71bbed3b7641b2d0785b65
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-06-04 11:50:24 -0600

    move default sequencer id to cmd package

--- cmd/sequencer/constants.go
+++ cmd/sequencer/constants.go
@@ -1,7 +1,10 @@
 package sequencer
 
+import "github.com/astria/astria-cli-go/cmd"
+
 const (
 	DefaultSequencerURL     = "http://127.0.0.1:26657"
+	DefaultSequencerChainID = cmd.DefaultLocalSequencerChainID
 	DefaultBridgeAssetID    = "transfer/channel-0/utia"
 	DefaultBridgeFeeAssetID = "nria"
 )
