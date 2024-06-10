64944b3a4a237a49f5d9e2e564f87dbd7a4f4d9b
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-06-10 16:53:10 -0600

    update service versions and their configs

--- cmd/devrunner/config/binaries_config_darwin_arm64.go
+++ cmd/devrunner/config/binaries_config_darwin_arm64.go
@@ -8,8 +8,8 @@ type Binary struct {
 }
 
 var Binaries = []Binary{
-	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v0.38.6/cometbft_0.38.6_darwin_arm64.tar.gz"},
-	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v0.13.0/astria-sequencer-aarch64-apple-darwin.tar.gz"},
-	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v0.7.0/astria-composer-aarch64-apple-darwin.tar.gz"},
-	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v0.16.0/astria-conductor-aarch64-apple-darwin.tar.gz"},
+	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v" + cometbftVersion + "/cometbft_" + cometbftVersion + "_darwin_arm64.tar.gz"},
+	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v" + sequencerVersion + "/astria-sequencer-aarch64-apple-darwin.tar.gz"},
+	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v" + composerVersion + "/astria-composer-aarch64-apple-darwin.tar.gz"},
+	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v" + conductorVersion + "/astria-conductor-aarch64-apple-darwin.tar.gz"},
 }
---
+++ cmd/devrunner/config/serviceVersions.go
@@ -0,0 +1,9 @@
+package config
+
+// NOTE: do not include the "v" prefix in the version
+const (
+	conductorVersion = "0.17.0"
+	composerVersion  = "0.7.0"
+	sequencerVersion = "0.13.0"
+	cometbftVersion  = "0.38.6"
+)
