139b3f82ec30b9448bfec8893532c1b755c7b5b3
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-06-25 14:36:22 -0600

    single config location for service versions

--- modules/cli/cmd/devrunner/config/binaries_config_linux_amd64.go
+++ modules/cli/cmd/devrunner/config/binaries_config_linux_amd64.go
@@ -8,8 +8,8 @@ type Binary struct {
 }
 
 var Binaries = []Binary{
-	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v0.38.6/cometbft_0.38.6_linux_amd64.tar.gz"},
-	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v0.13.0/astria-sequencer-x86_64-unknown-linux-gnu.tar.gz"},
-	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v0.7.0/astria-composer-x86_64-unknown-linux-gnu.tar.gz"},
-	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v0.17.0/astria-conductor-x86_64-unknown-linux-gnu.tar.gz"},
+	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v" + cometbft_version + "/cometbft_" + cometbft_version + "_linux_amd64.tar.gz"},
+	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v" + astria_sequencer_version + "/astria-sequencer-x86_64-unknown-linux-gnu.tar.gz"},
+	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v" + astria_composer_version + "/astria-composer-x86_64-unknown-linux-gnu.tar.gz"},
+	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v" + astria_conductor_version + "/astria-conductor-x86_64-unknown-linux-gnu.tar.gz"},
 }
---
+++ modules/cli/cmd/devrunner/config/service_versions.go
@@ -0,0 +1,9 @@
+package config
+
+// NOTE - do not include the 'v' at the beginning of the version number
+const (
+	cometbft_version         = "0.38.6"
+	astria_sequencer_version = "0.13.0"
+	astria_composer_version  = "0.7.0"
+	astria_conductor_version = "0.17.0"
+)
