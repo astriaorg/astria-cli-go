04c7cba8bffe5d4bc35942ee8f0b09c976d6be2c
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-08-01 14:30:56 -0600

    Feature(cli): Add service versions to `version` command (#136)
    * add service versions to version command
    
    * revert cli version command, add dev version command
    
    * fix versions after main merge
    
    * add versions alias

--- modules/cli/cmd/devrunner/config/binaries_config_darwin_arm64.go
+++ modules/cli/cmd/devrunner/config/binaries_config_darwin_arm64.go
@@ -8,8 +8,8 @@ type Binary struct {
 }
 
 var Binaries = []Binary{
-	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v" + cometbft_version + "/cometbft_" + cometbft_version + "_darwin_arm64.tar.gz"},
-	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v" + astria_sequencer_version + "/astria-sequencer-aarch64-apple-darwin.tar.gz"},
-	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v" + astria_composer_version + "/astria-composer-aarch64-apple-darwin.tar.gz"},
-	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v" + astria_conductor_version + "/astria-conductor-aarch64-apple-darwin.tar.gz"},
+	{"cometbft", "https://github.com/cometbft/cometbft/releases/download/v" + CometbftVersion + "/cometbft_" + CometbftVersion + "_darwin_arm64.tar.gz"},
+	{"astria-sequencer", "https://github.com/astriaorg/astria/releases/download/sequencer-v" + AstriaSequencerVersion + "/astria-sequencer-aarch64-apple-darwin.tar.gz"},
+	{"astria-composer", "https://github.com/astriaorg/astria/releases/download/composer-v" + AstriaComposerVersion + "/astria-composer-aarch64-apple-darwin.tar.gz"},
+	{"astria-conductor", "https://github.com/astriaorg/astria/releases/download/conductor-v" + AstriaConductorVersion + "/astria-conductor-aarch64-apple-darwin.tar.gz"},
 }
--- modules/cli/cmd/devrunner/config/service_versions.go
+++ modules/cli/cmd/devrunner/config/service_versions.go
@@ -2,8 +2,8 @@ package config
 
 // NOTE - do not include the 'v' at the beginning of the version number
 const (
-	cometbft_version         = "0.38.8"
-	astria_sequencer_version = "0.15.0"
-	astria_composer_version  = "0.8.1"
-	astria_conductor_version = "0.19.0"
+	CometbftVersion        = "0.38.8"
+	AstriaSequencerVersion = "0.15.0"
+	AstriaComposerVersion  = "0.8.1"
+	AstriaConductorVersion = "0.19.0"
 )
