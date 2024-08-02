1a92b964e75d4821f18b972d0fa378471f3d1e63
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-08-02 14:59:19 -0600

    dev version command now reads network config

--- modules/cli/cmd/devrunner/version.go
+++ modules/cli/cmd/devrunner/version.go
@@ -2,8 +2,12 @@ package devrunner
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/astriaorg/astria-cli-go/modules/cli/cmd"
 	"github.com/astriaorg/astria-cli-go/modules/cli/cmd/devrunner/config"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -16,15 +20,42 @@ var VersionCmd = &cobra.Command{
 }
 
 func seqVersionCmdHandler(c *cobra.Command, _ []string) {
-	// TODO: also load networks config and print the versions of the services
-	// specified there
-	fmt.Println("Default Service Versions:")
-	fmt.Println("cometbft:        ", "v"+config.CometbftVersion)
-	fmt.Println("astria-sequencer:", "v"+config.AstriaSequencerVersion)
-	fmt.Println("astria-composer: ", "v"+config.AstriaComposerVersion)
-	fmt.Println("astria-conductor:", "v"+config.AstriaConductorVersion)
+	flagHandler := cmd.CreateCliFlagHandler(c, cmd.EnvPrefix)
+
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		log.WithError(err).Error("Error getting home dir")
+		panic(err)
+	}
+	astriaDir := filepath.Join(homePath, ".astria")
+	instance := flagHandler.GetValue("instance")
+	config.IsInstanceNameValidOrPanic(instance)
+
+	network := flagHandler.GetValue("network")
+
+	networksConfigPath := filepath.Join(astriaDir, instance, config.DefaultNetworksConfigName)
+	networkConfigs := config.LoadNetworkConfigsOrPanic(networksConfigPath)
+
+	networkOverrides := networkConfigs.Configs[network]
+
+	fmt.Println("Preset Service Versions:")
+	// get longest service name for padding
+	longestServiceName := 0
+	for _, service := range networkOverrides.Services {
+		if len(service.Name) > longestServiceName {
+			longestServiceName = len(service.Name)
+		}
+	}
+	// print service versions
+	for _, service := range networkOverrides.Services {
+		fmt.Printf("%-*s: %s\n", longestServiceName, service.Name, service.Version)
+	}
 }
 
 func init() {
 	devCmd.AddCommand(VersionCmd)
+
+	flagHandler := cmd.CreateCliFlagHandler(VersionCmd, cmd.EnvPrefix)
+	flagHandler.BindStringFlag("network", config.DefaultTargetNetwork, "Select the network to run the services against. Valid networks are: local, dusk, dawn, mainnet")
+
 }
