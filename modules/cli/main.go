1b89050574b87d9ce4ba7f7331d4498949c29f90
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-08-14 15:52:39 -0600

    Remove `sudo` commands (#156)
    * remove sudo commands
    
    * remove main import for sudo package
    
    * remove sequencer internals

--- modules/cli/main.go
+++ modules/cli/main.go
@@ -5,7 +5,6 @@ import (
 	// NOTE - must import the commands to register them
 	_ "github.com/astriaorg/astria-cli-go/modules/cli/cmd/devrunner"
 	_ "github.com/astriaorg/astria-cli-go/modules/cli/cmd/sequencer"
-	_ "github.com/astriaorg/astria-cli-go/modules/cli/cmd/sequencer/sudo"
 )
 
 func main() {
