0b96cf6aa7802dd0a03b6fa4a0a235ab9cee1cf5
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-09-06 16:49:38 -0600

    refactor binaries info for dynamic download link creation

--- modules/cli/cmd/devrunner/config/binaries_config_darwin_arm64.go
+++ modules/cli/cmd/devrunner/config/binaries_config_darwin_arm64.go
@@ -3,37 +3,37 @@
 package config
 
 type Binary struct {
-	Name    string
-	Version string
-	Url     string
+	UrlPrefix string
+	UrlMiddle string
+	UrlSuffix string
 }
 
-type Binaries struct {
+type BinariesInfo struct {
 	CometBFT        Binary
 	AstriaSequencer Binary
 	AstriaComposer  Binary
 	AstriaConductor Binary
 }
 
-var KnownBinaries = Binaries{
+var DownloadUrlParts = BinariesInfo{
 	CometBFT: Binary{
-		Name:    "cometbft",
-		Version: "v" + CometbftVersion,
-		Url:     "https://github.com/cometbft/cometbft/releases/download/v" + CometbftVersion + "/cometbft_" + CometbftVersion + "_darwin_arm64.tar.gz",
+		UrlPrefix: "https://github.com/cometbft/cometbft/releases/download/v",
+		UrlMiddle: "/cometbft_",
+		UrlSuffix: "_darwin_arm64.tar.gz",
 	},
 	AstriaSequencer: Binary{
-		Name:    "astria-sequencer",
-		Version: "v" + AstriaSequencerVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/sequencer-v" + AstriaSequencerVersion + "/astria-sequencer-aarch64-apple-darwin.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/sequencer-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-sequencer-aarch64-apple-darwin.tar.gz",
 	},
 	AstriaComposer: Binary{
-		Name:    "astria-composer",
-		Version: "v" + AstriaComposerVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/composer-v" + AstriaComposerVersion + "/astria-composer-aarch64-apple-darwin.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/composer-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-composer-aarch64-apple-darwin.tar.gz",
 	},
 	AstriaConductor: Binary{
-		Name:    "astria-conductor",
-		Version: "v" + AstriaConductorVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/conductor-v" + AstriaConductorVersion + "/astria-conductor-aarch64-apple-darwin.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/conductor-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-conductor-aarch64-apple-darwin.tar.gz",
 	},
 }
--- modules/cli/cmd/devrunner/config/binaries_config_linux_amd64.go
+++ modules/cli/cmd/devrunner/config/binaries_config_linux_amd64.go
@@ -3,37 +3,37 @@
 package config
 
 type Binary struct {
-	Name    string
-	Version string
-	Url     string
+	UrlPrefix string
+	UrlMiddle string
+	UrlSuffix string
 }
 
-type Binaries struct {
+type BinariesInfo struct {
 	CometBFT        Binary
 	AstriaSequencer Binary
 	AstriaComposer  Binary
 	AstriaConductor Binary
 }
 
-var KnownBinaries = Binaries{
+var DownloadUrlParts = BinariesInfo{
 	CometBFT: Binary{
-		Name:    "cometbft",
-		Version: "v" + CometbftVersion,
-		Url:     "https://github.com/cometbft/cometbft/releases/download/v" + CometbftVersion + "/cometbft_" + CometbftVersion + "_linux_amd64.tar.gz",
+		UrlPrefix: "https://github.com/cometbft/cometbft/releases/download/v",
+		UrlMiddle: "/cometbft_",
+		UrlSuffix: "_linux_amd64.tar.gz",
 	},
 	AstriaSequencer: Binary{
-		Name:    "astria-sequencer",
-		Version: "v" + AstriaSequencerVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/sequencer-v" + AstriaSequencerVersion + "/astria-sequencer-x86_64-unknown-linux-gnu.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/sequencer-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-sequencer-x86_64-unknown-linux-gnu.tar.gz",
 	},
 	AstriaComposer: Binary{
-		Name:    "astria-composer",
-		Version: "v" + AstriaComposerVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/composer-v" + AstriaComposerVersion + "/astria-composer-x86_64-unknown-linux-gnu.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/composer-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-composer-x86_64-unknown-linux-gnu.tar.gz",
 	},
 	AstriaConductor: Binary{
-		Name:    "astria-conductor",
-		Version: "v" + AstriaConductorVersion,
-		Url:     "https://github.com/astriaorg/astria/releases/download/conductor-v" + AstriaConductorVersion + "/astria-conductor-x86_64-unknown-linux-gnu.tar.gz",
+		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/conductor-v",
+		UrlMiddle: "",
+		UrlSuffix: "/astria-conductor-x86_64-unknown-linux-gnu.tar.gz",
 	},
 }
