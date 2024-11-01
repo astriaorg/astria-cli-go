e8a24e6eacf3bbc518af99b9f0f50631fa77132f
Jesse Snyder <jessetsnyder@gmail.com>
2024-11-01 14:10:44 -0600

    move binary url related stuff to own binaries.go file

--- modules/cli/cmd/devrunner/config/binaries_config_darwin_amd64.go
+++ modules/cli/cmd/devrunner/config/binaries_config_darwin_amd64.go
@@ -2,38 +2,25 @@
 
 package config
 
-type UrlParts struct {
-	UrlPrefix string
-	UrlMiddle string
-	UrlSuffix string
-}
-
-type KnownServiceReleaseUrlsParts struct {
-	CometBFT        UrlParts
-	AstriaSequencer UrlParts
-	AstriaComposer  UrlParts
-	AstriaConductor UrlParts
-}
-
-var ServiceUrls = KnownServiceReleaseUrlsParts{
-	CometBFT: UrlParts{
-		UrlPrefix: "https://github.com/cometbft/cometbft/releases/download/v",
-		UrlMiddle: "/cometbft_",
-		UrlSuffix: "_darwin_amd64.tar.gz",
+var ServiceUrls = AvailableServiceReleaseUrls{
+	CometBFT: Url{
+		Prefix: "https://github.com/cometbft/cometbft/releases/download/v",
+		Middle: "/cometbft_",
+		Suffix: "_darwin_amd64.tar.gz",
 	},
-	AstriaSequencer: UrlParts{
-		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/sequencer-v",
-		UrlMiddle: "",
-		UrlSuffix: "/astria-sequencer-x86_64-apple-darwin.tar.gz",
+	AstriaSequencer: Url{
+		Prefix: "https://github.com/astriaorg/astria/releases/download/sequencer-v",
+		Middle: "",
+		Suffix: "/astria-sequencer-x86_64-apple-darwin.tar.gz",
 	},
-	AstriaComposer: UrlParts{
-		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/composer-v",
-		UrlMiddle: "",
-		UrlSuffix: "/astria-composer-x86_64-apple-darwin.tar.gz",
+	AstriaComposer: Url{
+		Prefix: "https://github.com/astriaorg/astria/releases/download/composer-v",
+		Middle: "",
+		Suffix: "/astria-composer-x86_64-apple-darwin.tar.gz",
 	},
-	AstriaConductor: UrlParts{
-		UrlPrefix: "https://github.com/astriaorg/astria/releases/download/conductor-v",
-		UrlMiddle: "",
-		UrlSuffix: "/astria-conductor-x86_64-apple-darwin.tar.gz",
+	AstriaConductor: Url{
+		Prefix: "https://github.com/astriaorg/astria/releases/download/conductor-v",
+		Middle: "",
+		Suffix: "/astria-conductor-x86_64-apple-darwin.tar.gz",
 	},
 }
